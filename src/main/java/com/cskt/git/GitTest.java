71f9c5bb199b5dffa3b1bdf7c6e13e2c47cffd54
psl <1994430140@qq.com>
2021-12-25 10:18:37 +0800

    2次提交

--- src/main/java/com/cskt/git/GitTest.java
+++ src/main/java/com/cskt/git/GitTest.java
@@ -1,7 +1,9 @@
 package com.cskt.git;
 
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("Hello,Git2!");
     }
 }
